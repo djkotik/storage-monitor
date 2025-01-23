d5774c4a7d319c545d3f83d64cc99a7012b68682
djkotik <scott@kotik.co.nz>
2025-01-23 13:39:23 +1300

    Add files via upload

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,10 +3,15 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
+  root: './frontend',  // Make sure Vite uses the correct root directory
   plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
+  },
+  build: {
+    outDir: '../dist', // Specify the output directory for production build
+    emptyOutDir: true,
   },
 })
