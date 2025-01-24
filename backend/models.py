a87f4bddd9ac493662108bc4fb15b08b74bd0829
djkotik <scott@kotik.co.nz>
2025-01-24 14:15:29 +1300

    Delete backend directory

--- backend/models.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-from datetime import datetime
-from typing import Dict, List
-
-class StorageStats(BaseModel):
-    timestamp: datetime
-    total_size: int
-    file_types: Dict[str, int]
-    folders: List[dict]
